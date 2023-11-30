0e43db938d239df3d11e7de264d11d9f17b654ec
romzifg <romzifg@gmail.com>
2023-11-30 21:31:56 +0700

    update, login screen base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import {
-  CustomButton,
-  DangerButton,
-  LinkButton,
-  PrimaryButton,
-} from "./components/Button";
-
 const App = () => {
   return (
     <div className="w-full h-screen flex justify-around items-center">
-      <PrimaryButton />
-      <DangerButton />
-      <LinkButton />
-      <CustomButton />
+      <div className="w-[800px] h-[600px] bg-gray-300 rounded-lg px-5 py-2">
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <h1 className="text-xl font-medium">Welcome to MyApp</h1>
+          <h1 className="text-md font-normal">Please enter your details</h1>
+        </div>
+      </div>
     </div>
   );
 };
